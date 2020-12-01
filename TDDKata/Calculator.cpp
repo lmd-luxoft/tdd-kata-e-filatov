777054be4ab489881253df26410a898e1d3d1d11
Евгений Филатов <e.filatov@owen.ua>
2020-12-01 11:12:12 +0200

    Первая версия функции

--- TDDKata/Calculator.cpp
+++ TDDKata/Calculator.cpp
@@ -8,5 +8,34 @@ int Calculator::Add(std::string expression)
 
 int Calculator::Add(char* expression)
 {
-    throw 0;
+    int cuttedNum = 0;
+    int result = 0;
+    char firstNumChar[16];
+    
+    if (!strlen(expression)) {
+        return -1; //Пустая строка
+    }
+
+    char* pos = 0;
+    pos = strchr(expression, ',');
+    if (!pos) {
+        return -2; //Не найден разделитель - недостаточно агрументов
+    }
+    if (pos == expression) {
+        return -2; //Нет первого аргумента
+    }
+
+    sscanf(expression, "%d", &cuttedNum);
+   
+    result += cuttedNum;
+
+    if (!strlen(++pos)) {
+        return -2; //Недостаточно аргументов
+    }
+
+    sscanf(pos, "%d", &cuttedNum);
+
+    result += cuttedNum;
+    
+    return result;
 }
