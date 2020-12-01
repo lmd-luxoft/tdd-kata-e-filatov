c31d1fbac519a5274e1afea926d24ed9121cf5f6
e-filatov <e.filatov@owen.ua>
2020-12-02 00:54:09 +0200

    Правки калькулятора для поддержки новой семантики

--- TDDKata/Calculator.cpp
+++ TDDKata/Calculator.cpp
@@ -14,23 +14,50 @@ int Calculator::Add(std::string expression)
 * -3 - Incorrect input string
 * -4 - Incorrect separator
 */
-
+#define SEPARATOR_DEFINEDER "//"
 int Calculator::Add(char* expression)
 {
     int cuttedNum = 0;
     int result = 0;
     int paramCount = 0;
     char firstNumChar[16];
+    char* pos = 0;
+    char separator = 0;
     
-    if (!strlen(expression)) {
+    if(!strlen(expression)) {
         return -1; //Пустая строка
     }
-    char* pos = expression;
-    while (pos = strchr(pos, '\n')) {
-        *pos = ',';
+
+    const char* det = strstr(expression, "//");
+    if(det == expression){
+    //if (1 == strcmp(expression, SEPARATOR_DEFINEDER)) {
+        if (expression[3] == '\n') {
+            separator = expression[2];
+            if ((separator < '0') || (separator > '9')) {
+                expression = &expression[4];
+                pos = expression;
+            }
+            else {
+                return -4;
+            }
+        }
+        else {
+            return -4;
+        }
+    }
+    else {
+        pos = expression;
+        while (pos = strchr(pos, '\n')) {
+            *pos = ',';
+        }
+        separator = ',';
     }
 
-    pos = strchr(expression, ',');
+    if (CheckStringOnIncorrectSymbols(expression, separator)) {
+        return -3;
+    }
+
+    pos = strchr(expression, separator);
     if (!pos) {
         return -2; //Не найден разделитель - недостаточно агрументов
     }
@@ -51,11 +78,20 @@ int Calculator::Add(char* expression)
             result += cuttedNum;
             paramCount++;
         }
-    } while (pos = strchr(pos, ','));
+    } while (pos = strchr(pos, separator));
 
     if (paramCount<2) {
         return -2; //Недостаточно аргументов
     }
 
     return result;
+}
+
+int Calculator::CheckStringOnIncorrectSymbols(char* input, char separator) {
+    do{
+        if (((*input < '0') || (*input > '9')) && (*input != separator)) {
+            return 1;
+        }
+        input++;
+    } while (*input);
 }
--- TDDKata/Calculator.h
+++ TDDKata/Calculator.h
@@ -18,5 +18,11 @@ public:
 	/// <param name="expression">Input expression</param>
 	/// <returns>Value of sum expression of expression</returns>
 	int Add(char* expression);
+
+private:
+
+	/// <summary>
+	/// We need to check that string containes only numbers and separators
+	int CheckStringOnIncorrectSymbols(char* input, char separator);
 };
 
